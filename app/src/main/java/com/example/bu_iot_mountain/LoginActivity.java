9706ed36845c3ad1352ef1b4930b35763a2db7ab
gimbeomjung <rla5451@naver.com>
2020-10-15 12:20:50 +0900

    회원가입페이지 추가 로그인,회원가입,마이페이지 버튼 연결 mainfests수정

--- app/src/main/java/com/example/bu_iot_mountain/LoginActivity.java
+++ app/src/main/java/com/example/bu_iot_mountain/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.bu_iot_mountain;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -11,4 +13,18 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+    public void gomypage(View v){
+        Intent i1;
+        i1 = new Intent(this, MypageActivity.class);
+        startActivity(i1);
+    }
+
+    public void gojoin(View v){
+        Intent i1;
+        i1 = new Intent(this, JoinActivity.class);
+        startActivity(i1);
+    }
+
+
 }
