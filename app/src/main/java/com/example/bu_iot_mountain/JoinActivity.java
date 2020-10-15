1d7b5c1ad289481a7844464bad01e5d5603dee6a
gimbeomjung <rla5451@naver.com>
2020-10-15 22:52:45 +0900

    아이디 중복확인 로직 추가

--- app/src/main/java/com/example/bu_iot_mountain/JoinActivity.java
+++ app/src/main/java/com/example/bu_iot_mountain/JoinActivity.java
@@ -2,13 +2,65 @@ package com.example.bu_iot_mountain;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class JoinActivity extends AppCompatActivity {
+
+    private final String dbName = "iotdb";
+    private final String tableName = "member";
+
+    EditText idText;
+    EditText passText;
+    String Id;
+    String Pass;
+
+    SQLiteDatabase iotdb;
+    String id;
+    String pw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join);
+
+        idText = (EditText) findViewById(R.id.id);
+        passText = (EditText) findViewById(R.id.pass);
+
     }
+
+
+    public void members(View v) {
+
+        SQLiteDatabase ReadDB = this.openOrCreateDatabase(dbName, MODE_PRIVATE, null);
+
+
+        //SELECT문을 사용하여 테이블에 있는 데이터를 가져옵니다.. 아이디 중복검색
+        Cursor c = ReadDB.rawQuery("SELECT * FROM " + tableName, null);
+        if(c == null){//중복없음 가입가능
+
+            id = idText.getText().toString();
+            pw = passText.getText().toString();
+
+            iotdb.execSQL("INSERT INTO " + tableName + "(name, age, num, major) VALUES" +
+                    "("+"'"+id+"'"+","+pw+");");
+
+            Toast.makeText(this,"table :" + tableName + "생성",Toast.LENGTH_SHORT).show();
+
+        }else{
+
+        }
+
+
+        c.close();
+
+
+    }
+
+
 }
--- app/src/main/java/com/example/bu_iot_mountain/LoginActivity.java
+++ app/src/main/java/com/example/bu_iot_mountain/LoginActivity.java
@@ -3,15 +3,66 @@ package com.example.bu_iot_mountain;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private final String dbName = "iotdb";
+    private final String tableName = "member";
+
+    SQLiteDatabase sampleDB = null;
+
+    EditText idText;
+    EditText editTextTextPassword;
+    String Id;
+    String Pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Id = idText.getText().toString();
+        Pass = editTextTextPassword.getText().toString();
+
+
+        try {
+
+
+            sampleDB = this.openOrCreateDatabase(dbName, MODE_PRIVATE, null);
+
+            //테이블이 존재하지 않으면 새로 생성합니다.
+            sampleDB.execSQL("CREATE TABLE IF NOT EXISTS " + tableName
+                    + " (id VARCHAR(20), pw VARCHAR(20) );");
+
+            //테이블이 존재하는 경우 기존 데이터를 지우기 위해서 사용합니다.
+            //sampleDB.execSQL("DELETE FROM " + tableName  );
+
+            //새로운 데이터를 테이블에 집어넣습니다..
+            /*for (int i=0; i<names.length; i++ ) {
+                sampleDB.execSQL("INSERT INTO " + tableName
+                        + " (name, phone)  Values ('" + names[i] + "', '" + phones[i]+"');");
+            }*/
+            Toast.makeText(getApplicationContext(),  "디비생성", Toast.LENGTH_LONG).show();
+
+            sampleDB.close();
+
+        } catch (SQLiteException se) {
+            Toast.makeText(getApplicationContext(),  se.getMessage(), Toast.LENGTH_LONG).show();
+            Log.e("", se.getMessage());
+
+
+        }
+
+
+
+
     }
 
     public void gomypage(View v){
