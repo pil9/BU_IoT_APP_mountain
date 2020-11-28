c9a938280bde896fb6b9b2279aa04719b25bc7b0
gimbeomjung <rla5451@naver.com>
2020-11-28 23:56:09 +0900

    회원가입페이지 추가 로그인,회원가입,마이페이지 버튼 연결 mainfests수정

--- app/src/main/java/com/example/bu_iot_mountain/NoteDetailActivity.java
+++ app/src/main/java/com/example/bu_iot_mountain/NoteDetailActivity.java
@@ -38,7 +38,7 @@ public class NoteDetailActivity extends AppCompatActivity {
 
         while (cursor.moveToNext()){//다음 레코드가 있을동안 수행
             title.setText(cursor.getString(1));//제목데이터 텍스트뷰에 넣어주기
-            context.setText(cursor.getString(2));//내용데이터 텍스트뷰에 넣어주기 
+            context.setText(cursor.getString(2));//내용데이터 텍스트뷰에 넣어주기
         }
         //Toast.makeText(getApplicationContext(),"로그확인.idx: "+strNames+" id:"+strNumbers+" pw: "+strNumbers2+"",Toast.LENGTH_LONG).show();
         cursor.close();
