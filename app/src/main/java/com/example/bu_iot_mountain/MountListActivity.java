f63938661dee4db751c21655f140ffb9ba38b76c
rla5451 <rla5451@naver.com>
2020-12-19 19:57:13 +0900

    1220 인증글리스트 페이지 개발

--- app/src/main/java/com/example/bu_iot_mountain/MountListActivity.java
+++ app/src/main/java/com/example/bu_iot_mountain/MountListActivity.java
@@ -3,8 +3,12 @@ package com.example.bu_iot_mountain;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -12,16 +16,22 @@ import java.util.List;
 
 public class MountListActivity extends AppCompatActivity {
 
+    LoginActivity.myDBHelper myHelper;
+    SQLiteDatabase sqlDB;//쿼리문 수행용
+
     private ListView noticeListView;
+    private List<Notice> noticeList = new ArrayList<Notice>();
+    // static LinkedList<String> notelistmini = new LinkedList();
     private NoticeListAdt Adapter;
-    private List<Notice> noticeList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_list);
 
-        noticeListView = (ListView) findViewById(R.id.noticeListView);
+        myHelper=new LoginActivity.myDBHelper(this);
+
+        /*noticeListView = (ListView) findViewById(R.id.noticeListView);
         noticeList = new ArrayList<Notice>();
         noticeList.add(new Notice("1번게시글","김철수","2020-10-24"));
         noticeList.add(new Notice("2번게시글","박영희","2020-10-24"));
@@ -31,7 +41,54 @@ public class MountListActivity extends AppCompatActivity {
 
 
         Adapter = new NoticeListAdt(getApplicationContext(), noticeList);
+        noticeListView.setAdapter(Adapter);*/
+
+        noticeListView = (ListView) findViewById(R.id.noticeListView);
+        Adapter = new NoticeListAdt(getApplicationContext(), noticeList);
         noticeListView.setAdapter(Adapter);
+        mininotelist();
+        noticeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // 상세정보 화면으로 이동하기(인텐트 날리기)
+                // 1. 다음화면을 만든다
+                // 2. AndroidManifest.xml 에 화면을 등록한다
+                // 3. Intent 객체를 생성하여 날린다
+                Intent intent = new Intent(
+                        getApplicationContext(), // 현재화면의 제어권자
+                        NoteDetailActivity.class); // 다음넘어갈 화면
+
+                // intent 객체에 데이터를 실어서 보내기
+                // 리스트뷰 클릭시 인텐트 (Intent) 생성하고 position 값을 이용하여 인텐트로 넘길값들을 넘긴다
+                //Log.d(TAG, "detail idx::::::"+noticeList.get(position).date);
+                intent.putExtra("detailnidx", noticeList.get(position).date);
+
+                //TextView nnnidx =(TextView)findViewById(R.id.dateText);
+                //String nsidx = nnnidx.getText().toString();
+                /*Intent i1;
+                i1 = new Intent(this, NoteDetailActivity.class);
+                i1.putExtra("detailnidx", nsidx);*/
+                startActivity(intent);
+
+            }
+        });
+
+
+    }
+
+    public void mininotelist() {
+        sqlDB = myHelper.getReadableDatabase();
+        Cursor cursor2;
+
+        cursor2 = sqlDB.rawQuery("SELECT * FROM note left OUTER join member on note.useridx=member.midx order by nidx desc;", null);
+        while (cursor2.moveToNext()){
+            noticeList.add(new Notice(cursor2.getString(8),cursor2.getString(1),cursor2.getString(0)));
+        }
+
+        cursor2.close();
+        sqlDB.close();
+
     }
 
     public void gomypage(View v){
